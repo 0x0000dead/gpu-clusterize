b99da2c81bca2202077dbbfa8bcacdc0d2e508c4
Sergey V <41781097+0x0000dead@users.noreply.github.com>
2022-12-15 02:19:42 +0700

    every work

--- cpp/dist.cu
+++ cpp/dist.cu
@@ -2,7 +2,6 @@
 #include <device_launch_parameters.h>
 #include <vector>
 #include <iostream>
-const int N(10);
 
 #define check(ans) { _check((ans), __FILE__, __LINE__); }
 inline void _check(cudaError_t code, char* file, int line)
@@ -17,42 +16,43 @@ int div_up(int a, int b) {
     return ((a % b) != 0) ? (a / b + 1) : (a / b);
 }
 
-__global__ void calc_distances_kernel(double* distances, double** atoms, int num_dimensions);
+__global__ void calc_distances_kernel(double* distances, double* atoms, int num_dimensions, int M);
 
-__global__ void calc_distances_kernel(double* distances, double* atoms, int num_dimensions)
+__global__ void calc_distances_kernel(double* distances, double* atoms, int num_dimensions, int M)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (idx >= N) return;
+    if (idx >= M) return;
 
-    for (int j = idx + 1; j < N; ++j) {
+    for (int j = idx + 1; j < M; ++j) {
         double sum = 0;
         for (int k = 0; k < num_dimensions; ++k) {
             sum+= (atoms[idx * num_dimensions + k] - atoms[j * num_dimensions + k]) * (atoms[idx * num_dimensions + k] - atoms[j * num_dimensions + k]);
         }
-        distances[idx * N + j] = sqrt(sum);
-        distances[j * N + idx] = sqrt(sum);
+        distances[idx * M + j] = sqrt(sum);
+        distances[j * M + idx] = sqrt(sum);
     }
+
 }
 
-void calculate_distance_matrix_cuda(double *input_ij_1d, double *output_ij_1d, int num_dimensions) {
+void calculate_distance_matrix_cuda(double *input_ij_1d, double *output_ij_1d, int num_dimensions, int M) {
     double* distances_d;
     double* all_points_d;
 
-    check(cudaMalloc(&distances_d, N * N * sizeof(double)));
-    check(cudaMalloc(&all_points_d, N * num_dimensions * sizeof(double)));
-    check(cudaMemcpy(all_points_d, input_ij_1d, N * num_dimensions * sizeof(double), cudaMemcpyHostToDevice));
+    check(cudaMalloc(&distances_d, M * M * sizeof(double)));
+    check(cudaMalloc(&all_points_d, M * num_dimensions * sizeof(double)));
+    check(cudaMemcpy(all_points_d, input_ij_1d, M * num_dimensions * sizeof(double), cudaMemcpyHostToDevice));
 
     const int threads_per_block(512);
-    dim3 n_blocks(div_up(N, threads_per_block));
+    dim3 n_blocks(div_up(M, threads_per_block));
 
-    calc_distances_kernel <<< n_blocks, threads_per_block >>> (distances_d, all_points_d, num_dimensions);
+    calc_distances_kernel <<< n_blocks, threads_per_block >>> (distances_d, all_points_d, num_dimensions, M);
 
     check(cudaPeekAtLastError());
     check(cudaDeviceSynchronize());
 
 
-    check(cudaMemcpy(output_ij_1d, distances_d, N * N * sizeof(double), cudaMemcpyDeviceToHost));
+    check(cudaMemcpy(output_ij_1d, distances_d, M * M * sizeof(double), cudaMemcpyDeviceToHost));
 
 
     check(cudaFree(all_points_d));
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,3 +1,5 @@
+#define PRINT_DEBUG 1
+
 #include <iostream>
 #include <vector>
 #include <tuple>
@@ -7,116 +9,119 @@
 #include <istream>
 #include "KMedoids.h"
 #include <immintrin.h>
-//#include "dist.cu"
-
-using namespace std;
-void calculate_distance_matrix_cuda(double *input_ij_1d, double *output_ij_1d, int num_dimensions);
-
-std::vector<std::vector<double>> calculate_distance_matrix(std::vector<std::vector<double>> &all_points) {
-    int size = all_points.size();
-
-    std::vector<std::vector<double>> distance_matrix(size, std::vector<double>(size));
-
-    for (int i = 0; i < size; ++i) {
-        for (int j = i+1; j < size; ++j) {
-            double sum = 0;
-            for (int k = 0; k < all_points[i].size(); ++k) {
-                sum += (all_points[i][k] - all_points[j][k]) * (all_points[i][k] - all_points[j][k]);
-            }
-            distance_matrix[i][j] = sqrt(sum);
-            distance_matrix[j][i] = sqrt(sum);
-        }
-        distance_matrix[i][i] = INT_MAX;
-    }
-    return distance_matrix;
-}
-
-#include <immintrin.h>
-#include <cstdint>
 #include <cmath>
-#include <vector>
-
-void print_matrix(std::vector<std::vector<double>> &matrix) {
-    for (int i = 0; i < matrix.size(); ++i) {
-        for (int j = 0; j < matrix[i].size(); ++j) {
-            std::cout << matrix[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
 #include <string>
 #include <strstream>
-std::vector<std::vector<double>> read_distance_matrix(std::ifstream& rfile, int m ) {
-    std::vector<std::vector<double>> distance_matrix(m, std::vector<double>(m, 0));
-    if (rfile.is_open()) {
-        std::string line;
-        int i = 0;
-        while (std::getline(rfile, line)) {
-            std::strstream ss;
-            ss << line;
-            for (int j = 0; j < m; ++j) {
-                ss >> distance_matrix[i][j];
-            }
-            ++i;
-            std::cout << i << std::endl;
-        }
+#include <chrono>
 
+using namespace std;
+
+void calculate_distance_matrix_cuda(double *input_ij_1d, double *output_ij_1d, int num_dimensions, int M);
+
+namespace MATRIX {
+    std::vector <std::vector<double>> calculate_distance_matrix(std::vector <std::vector<double>> &all_points) {
+        int size = all_points.size();
+
+        std::vector <std::vector<double>> distance_matrix(size, std::vector<double>(size));
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = i + 1; j < size; ++j) {
+                double sum = 0;
+                for (int k = 0; k < all_points[i].size(); ++k) {
+                    sum += (all_points[i][k] - all_points[j][k]) * (all_points[i][k] - all_points[j][k]);
+                }
+                distance_matrix[i][j] = sqrt(sum);
+                distance_matrix[j][i] = sqrt(sum);
+            }
+            distance_matrix[i][i] = INT_MAX;
+        }
+        return distance_matrix;
     }
+
+
+    void print_matrix(std::vector <std::vector<double>> &matrix) {
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[i].size(); ++j) {
+                std::cout << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    std::vector <std::vector<double>> read_distance_matrix(std::ifstream &rfile, int m) {
+        std::vector <std::vector<double>> distance_matrix(m, std::vector<double>(m, 0));
+        if (rfile.is_open()) {
+            std::string line;
+            int i = 0;
+            while (std::getline(rfile, line)) {
+                std::strstream ss;
+                ss << line;
+                for (int j = 0; j < m; ++j) {
+                    ss >> distance_matrix[i][j];
+                }
+                ++i;
+                std::cout << i << std::endl;
+            }
+
+        }
         rfile.close();
-    return distance_matrix;
+        return distance_matrix;
+    }
 }
 
-#include <chrono>
 int main() {
     std::ifstream file("../../data/my_data_10.txt");
 //    std::ifstream file("../../data/ds1x4.txt");
-    std::ifstream file_distance("../../distance_matrix.txt");
+//    std::ifstream file_distance("../../distance_matrix.txt");
 
     if (file.is_open()) {
         int dim, m;
 //        double UB = 30;
         double UB = 11934.8;
 //        double UB = 100000;
-        int k = 64;
+        int k = 2;
 //        int k = 5;
 
         file >> m >> dim;
         std::vector<std::vector<double>> input_ij(m, std::vector<double>(m, 0));
-        // 1d array of doulbe
+
         double *input_ij_1d = new double[m*m];
         double *distance_matrix_1d = new double[m*m];
-        // double array
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < dim; j++) {
                 file >> input_ij[i][j];
                 input_ij_1d[i*dim + j] = input_ij[i][j];
-                std::cout << input_ij_1d[i*dim + j] << " ";
             }
-            std::cout << std::endl;
         }
         auto start = std::chrono::high_resolution_clock::now();
-        calculate_distance_matrix_cuda(input_ij_1d, distance_matrix_1d, dim);
-        vector<vector<double>> d_ij;
+        calculate_distance_matrix_cuda(input_ij_1d, distance_matrix_1d, dim, m);
+        free(input_ij_1d);
+
+        std::vector<std::vector<double>> d_ij(m, std::vector<double>(m, 0));
+
         for (int i = 0; i < m; i++) {
             vector<double> row;
             for (int j = 0; j < m; j++) {
                 row.push_back(distance_matrix_1d[i*m + j]);
+                d_ij[i][j] = distance_matrix_1d[i*m + j];
+#if PRINT_DEBUG
                 std::cout << distance_matrix_1d[i*m + j] << " ";
+#endif
             }
+#if PRINT_DEBUG
             std::cout << std::endl;
-            d_ij.push_back(row);
+#endif
         }
-//        vector<vector<double>> d_ij = read_distance_matrix(file_distance, m);
+//        vector<vector<double>> d_ij = MATRIX::read_distance_matrix(file_distance, m);
         auto finish = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed = finish - start;
         std::cout << "Time matrix calculation: " << elapsed.count() << " s\n";
 
-//        print_matrix(d_ij);
         KMedoids kmed(k, m, d_ij, UB);
-         start = std::chrono::high_resolution_clock::now();
+        start = std::chrono::high_resolution_clock::now();
         std::vector<double> res = kmed.solve(true);
-         finish = std::chrono::high_resolution_clock::now();
-
+        finish = std::chrono::high_resolution_clock::now();
 
         elapsed = finish - start;
         std::cout << "LB: " << res[0] << std::endl;
